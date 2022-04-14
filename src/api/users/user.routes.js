2ef073ccee47e92e8c938ded4e698e3feae2b922
Esteve Segura <esteve.segura@voicemod.net>
2022-04-14 22:53:45 +0200

    Apply rate limit to sensitive endpoints

--- src/api/users/user.routes.js
+++ src/api/users/user.routes.js
@@ -1,11 +1,18 @@
 const UserRoutes = require('express').Router();
 const { authorize } = require('../../middleware/authorize');
+const rateLimit = require('express-rate-limit')
 
 const { getById, create, authenticate, getUserData } = require('./user.controller');
+const userCreateRateLimit = rateLimit({
+	windowMs: 5 * 60 * 1000, // 5min
+	max: 10,
+	standardHeaders: true,
+	legacyHeaders: false,
+});
 
 UserRoutes.get('/', [authorize], getUserData);
 UserRoutes.get('/:id', [authorize], getById);
-UserRoutes.post('/', create);
+UserRoutes.post('/', [userCreateRateLimit], create);
 UserRoutes.post('/login', authenticate);
 
 module.exports = UserRoutes;
